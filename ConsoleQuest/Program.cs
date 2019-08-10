278665a73bf550f771df55ccd5df7f92588203be
Rixium <gamerpotty@gmail.com>
2019-08-10 20:54:30 +0100

    Add system console usage

--- ConsoleQuest/Program.cs
+++ ConsoleQuest/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ConsoleQuest
+﻿using System;
+using ConsoleQuest.Console;
+using ConsoleQuest.Console.Consoles;
+using ConsoleQuest.Console.Data;
+
+namespace ConsoleQuest
 {
     public static class Program
     {
+        private static IConsole _console;
+
         static void Main(string[] args)
         {
+            _console = new SystemConsole();
+            _console.QueueData(new ConsoleData("Hello World!"));
+            _console.Flush();
+            System.Console.WriteLine(_console.GetLast().Read());
         }
     }
 }
