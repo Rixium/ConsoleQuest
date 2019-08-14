182a8b1303cdb87c30057b3f037e043ca8610eec
Rixium <gamerpotty@gmail.com>
2019-08-14 09:45:58 +0100

    Add examples project

---
+++ ConsoleQuest.Examples.Town/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleQuest.World;
+
+namespace ConsoleQuest.Examples.Town
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var worldLoader = new WorldLoader();
+            var world = worldLoader.Load<World.World>("resources/world.json");
+            System.Console.WriteLine(world);
+        }
+    }
+}
